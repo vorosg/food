f74a06f2aac2a92c4ea7400f8b5a8cd01eb88cf2
Gergo Voros <v.gergo1993@gmail.com>
2024-01-15 22:54:53 +0100

    Utilizing suspense after partially rendering the page. And then streams in and displays the content once it has been loaded

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import getMeals from "@/lib/meals";
+import { Suspense } from "react";
 
-const Meals = async() => {
+const FetchMeals = async() => {
     const meals = await getMeals();
+    return <MealsGrid meals={meals} />
+}
+
+const Meals = () => {
     return (
         <>
             <header className={classes.header}>
@@ -17,7 +22,9 @@ const Meals = async() => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <FetchMeals />
+                </Suspense>
             </main>
         </>
     )
