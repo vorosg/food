de655e60eb9f74e011b215081818e4e13eab94e0
Gergo Voros <v.gergo1993@gmail.com>
2024-01-15 22:34:35 +0100

    Fetching Data By Levaraging NextJS & Fullstack Capabilities

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import getMeals from "@/lib/meals";
 
-const Meals = () => {
+const Meals = async() => {
+    const meals = await getMeals();
     return (
         <>
             <header className={classes.header}>
@@ -15,7 +17,7 @@ const Meals = () => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
     )
