26363d9a4724bfe48f9b4e9503579a0ee8d2a9ba
Gergo Voros <v.gergo1993@gmail.com>
2024-01-22 14:04:21 +0100

    Add not found for specific meals route and create init form for meal share

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,14 @@
 import Image from 'next/image';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals'
+import { notFound } from 'next/navigation';
 
 const SelectedMeal = ({ params }) => {
     const meal = getMeal(params.slug);
+
+    if(!meal) {
+        notFound();
+    }
 
     meal.instructions = meal.instructions.replace(/\n/g, '<br />');
 
