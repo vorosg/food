1a4687ba12e40e86941029cf12fe65a35f027d6c
Gergo Voros <v.gergo1993@gmail.com>
2024-01-25 23:14:32 +0100

    Add Preview to Picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,20 +1,47 @@
 'use client';
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 const ImagePicker = ({ label, name }) => {
 
+    const [pickedImage, setPickedImage] = useState('');
     const imageInputRef = useRef();
 
     const handlePickClick = () => {
         imageInputRef.current.click();
     };
 
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    }
+
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No Image picked yet!</p>}
+                    {pickedImage && (
+                        <Image 
+                          src={pickedImage} 
+                          alt="The image selected by the user." 
+                          fill
+                        />
+                    )}
+                </div>
                 <input 
                     className={classes.input}
                     type="file" 
@@ -22,6 +49,8 @@ const ImagePicker = ({ label, name }) => {
                     accept="image/png image/jpeg"
                     name={name} 
                     ref={imageInputRef}
+                    onChange={handleImageChange}
+                    required
                 />
                 <button className={classes.button} type="button" onClick={handlePickClick}>
                     Pick an Image
